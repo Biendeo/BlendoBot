165f14e9ef99af30ab64ff57f330b0581cf8a83f
Thomas Moffet <pantherman1996@gmail.com>
2019-03-03 11:01:33 +1100

    Printed the start and end times of the matches.

--- Modules/OverwatchLeague/src/OverwatchLeague.cs
+++ Modules/OverwatchLeague/src/OverwatchLeague.cs
@@ -16,7 +16,7 @@ namespace OverwatchLeague {
 			Term = "?overwatchleague",
 			Name = "Overwatch League",
 			Description = "Tells you up-to-date stats about the Overwatch League.",
-			Usage = $"Usage:\n{"?overwatchleague live".Code()} {"(stats about the match that is currently on)".Italics()}\n{"?overwatchleague next".Code()} {"(stats about the match that is currently on)".Italics()}",
+			Usage = $"Usage:\n{"?overwatchleague live".Code()} {"(stats about the match that is currently on)".Italics()}\n{"?overwatchleague next".Code()} {"(stats about the next match that will be played)".Italics()}\nAll times listed are in UTC.",
 			Author = "Biendeo",
 			Version = "0.1.0",
 			Startup = async () => { await Task.Delay(0); return true; },
@@ -27,14 +27,28 @@ namespace OverwatchLeague {
 			var sb = new StringBuilder();
 
 			sb.Append("```");
-
-			sb.AppendLine("Current Match:");
-
 			var homeTeam = match.competitors[0];
 			var awayTeam = match.competitors[1];
 
 			var currentHomeScore = match.scores[0].value;
 			var currentAwayScore = match.scores[1].value;
+
+			sb.AppendLine($"Planned time: {new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds((double)match.startDateTS).ToString("d/MM/yyyy h:mm:ss tt K")} - {new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds((double)match.endDateTS).ToString("d/MM/yyyy h:mm:ss tt K")}");
+			sb.Append("Real time: ");
+
+			try {
+				sb.Append(new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds((double)match.actualStartDate).ToString("d/MM/yyyy h:mm:ss tt K"));
+				sb.Append(" - ");
+			} catch (RuntimeBinderException) {
+				sb.Append("??? - ");
+			}
+
+			try {
+				sb.Append(new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds((double)match.actualEndDate).ToString("d/MM/yyyy h:mm:ss tt K"));
+			} catch (RuntimeBinderException) {
+				sb.Append("???");
+			}
+			sb.AppendLine();
 
 			sb.AppendLine($"{homeTeam.name} vs. {awayTeam.name}");
 			sb.AppendLine($"{currentHomeScore} - {currentAwayScore}");
