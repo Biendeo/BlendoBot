81c80c6f0910a0046b5d20f3124b22ef8f3705c6
Thomas Moffet <pantherman1996@gmail.com>
2019-04-03 17:13:31 +1100

    A better way of requiring fields.

--- BlendoBot/src/Config.cs
+++ BlendoBot/src/Config.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.Entities;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,8 +13,9 @@ namespace BlendoBot {
 	/// <summary>
 	/// Contains the private properties of the config.
 	/// </summary>
-	[JsonObject(MemberSerialization.OptOut)]
+	[JsonObject(MemberSerialization.OptIn)]
 	internal class ConfigPrivate {
+		[JsonProperty(Required = Required.Always)]
 		public string Token;
 	}
 
@@ -22,12 +24,17 @@ namespace BlendoBot {
 	/// </summary>
 	[JsonObject(MemberSerialization.OptOut)]
 	internal class ConfigPublic {
+		[JsonProperty(Required = Required.Always)]
 		public string Name;
+		[JsonProperty(Required = Required.Always)]
 		public string Version;
+		[JsonProperty(Required = Required.Always)]
 		public string Description;
+		[JsonProperty(Required = Required.Always)]
 		public string Author;
+		[JsonProperty(Required = Required.Always)]
 		public string ActivityName;
-		[JsonIgnore]
+		[JsonProperty(Required = Required.Always), JsonConverter(typeof(StringEnumConverter))]
 		public ActivityType ActivityType;
 	}
 
@@ -37,9 +44,9 @@ namespace BlendoBot {
 	/// </summary>
 	[JsonObject(MemberSerialization.OptIn)]
 	public class Config {
-		[JsonProperty]
+		[JsonProperty(Required = Required.Always)]
 		internal ConfigPrivate Private;
-		[JsonProperty]
+		[JsonProperty(Required = Required.Always)]
 		internal ConfigPublic Public;
 
 		public string Name { get { return Public.Name; } }
@@ -60,12 +67,14 @@ namespace BlendoBot {
 				Console.Error.WriteLine($"Config.FromJson() can't find {filePath}! Aborting program...");
 				return null;
 			}
-			Config c = JsonConvert.DeserializeObject<Config>(File.ReadAllText(filePath));
-			if (c.Name == null || c.Version == null || c.Description == null || c.Author == null || c.ActivityName == null || c.Private.Token == null) {
+			try {
+				Config c = JsonConvert.DeserializeObject<Config>(File.ReadAllText(filePath));
+				return c;
+			} catch (JsonSerializationException exc) {
 				Console.Error.WriteLine("Config.FromJson() read in an incomplete json. You need to add in all the fields!");
+				Console.Error.WriteLine(exc);
 				return null;
 			}
-			return c;
 		}
 	}
 }
