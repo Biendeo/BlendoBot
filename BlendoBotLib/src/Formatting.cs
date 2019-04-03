fa3ab4af49e38253b567a4f91301c013bfd98bf7
Thomas Moffet <pantherman1996@gmail.com>
2019-04-03 14:52:49 +1100

    Added small checks such that formatting is not applied to empty strings.

--- BlendoBotLib/src/Formatting.cs
+++ BlendoBotLib/src/Formatting.cs
@@ -5,27 +5,27 @@ using System.Text;
 namespace BlendoBotLib {
 	public static class Formatting {
 		public static string Bold(this string s) {
-			return $"**{s}**";
+			return s == "" ? s : $"**{s}**";
 		}
 
 		public static string Italics(this string s) {
-			return $"*{s}*";
+			return s == "" ? s : $"*{s}*";
 		}
 
 		public static string Underline(this string s) {
-			return $"__{s}__";
+			return s == "" ? s : $"__{s}__";
 		}
 
 		public static string Strikeout(this string s) {
-			return $"~~{s}~~";
+			return s == "" ? s : $"~~{s}~~";
 		}
 
 		public static string Code(this string s) {
-			return $"`{s}`";
+			return s == "" ? s : $"`{s}`";
 		}
 
 		public static string CodeBlock(this string s) {
-			return $"```{s}```";
+			return s == "" ? s : $"```{s}```";
 		}
 	}
 }
