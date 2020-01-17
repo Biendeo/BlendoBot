bfabe09a29c37e98fa3db7c1af09202219762301
Thomas Moffet <pantherman1996@gmail.com>
2020-01-18 10:56:37 +1100

    Turns out it really doesn't like performing the same action on multiple platforms. This should at least make it consistent.

---
+++ Modules/DecimalSpiral/prebuild.py
@@ -0,0 +1,7 @@
+﻿import subprocess
+
+hash = subprocess.check_output("git log --pretty=format:%h -n 1 *", shell=True).decode("utf-8").strip()
+date = subprocess.check_output("git show --no-patch --no-notes --pretty=%cd", shell=True).decode("utf-8").strip()
+
+with open("version.txt", "w") as f:
+	f.write("{} - {}".format(hash, date))
