3cf206068045448c1fb1d725e832a4ace7ce0a5d
Thomas Moffet <pantherman1996@gmail.com>
2020-01-21 11:38:21 +1100

    Reworked the roll command to be much more usable and approachable. The limits can be tweaked in the future but this seems pretty reasonable.

--- Modules/Roll/src/Roll.cs
+++ Modules/Roll/src/Roll.cs
@@ -12,107 +12,182 @@ namespace Roll {
 
 		public override string Term => "?roll";
 		public override string Name => "Roll";
-		public override string Description => "Rolls a given dice a given number of times.";
-		public override string Usage => $"Usage: {"?random [dice value] [optional: num rolls = 1]".Code()}\nIf you request 20 or fewer rolls, I'll print out all the individual dice numbers. Otherwise, I'll give you the five number summary.";
+		public override string Description => "Simulates dice rolls and coin flips";
+		public override string Usage => $"Usage ({$"where {"x".Code()} and {"y".Code()} are positive integers".Italics()}):\n{"?random [y]".Code()} ({$"rolls a {"y".Code()}-sided dice, giving a value between 1 and {"y".Code()}".Italics()})\n{"?roll d[y]".Code()} ({$"same as {"?roll y".Code()}".Italics()})\n{"?roll [x]d[y]".Code()} ({$"rolls a {"y".Code()}-sided dice {"x".Code()} number of times".Italics()})\n{"?roll coin".Code()} ({"returns either heads or tails".Italics()})";
 		public override string Author => "Biendeo";
-		public override string Version => "0.2.0";
+		public override string Version => "0.3.0";
+
+		private Random random;
 
 		public override async Task<bool> Startup() {
+			random = new Random();
 			await Task.Delay(0);
 			return true;
 		}
 
 		public override async Task OnMessage(MessageCreateEventArgs e) {
 			string[] splitMessage = e.Message.Content.Split(' ');
-			// We want to make sure that there's either two or three arguments.
-			if (splitMessage.Length < 2) {
-				await BotMethods.SendMessage(this, new SendMessageEventArgs {
-					Message = $"Too few arguments specified to `?roll`",
-					Channel = e.Channel,
-					LogMessage = "RollErrorTooFewArgs"
-				});
-			} else if (splitMessage.Length > 3) {
-				await BotMethods.SendMessage(this, new SendMessageEventArgs {
-					Message = $"Too many arguments specified to `?roll`",
-					Channel = e.Channel,
-					LogMessage = "RollErrorTooManyArgs"
-				});
-			} else {
-				// If two arguments are given, then we are just rolling a dice one time.
-				int rollCount = 1;
-				if (!int.TryParse(splitMessage[1], out int diceValue) || (diceValue <= 0)) {
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = $"The dice value given is not a positive integer",
-						Channel = e.Channel,
-						LogMessage = "RollErrorFirstArgInvalid"
-					});
-					return;
-				}
-				if (splitMessage.Length == 3 && (!int.TryParse(splitMessage[2], out rollCount) || rollCount <= 0)) {
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = $"The roll count is not a positive integer",
-						Channel = e.Channel,
-						LogMessage = "RollErrorSecondArgInvalid"
-					});
-					return;
-				}
-				if (rollCount > 1000000) {
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = $"The roll count is above 1000000, and I don't want to do that",
-						Channel = e.Channel,
-						LogMessage = "RollErrorSecondArgTooLarge"
-					});
-					return;
-				}
-				var results = new List<int>(rollCount);
-				var random = new Random();
-				for (int count = 0; count < rollCount; ++count) {
-					// The int cast should truncate the double.
-					int currentRoll = (int)(random.NextDouble() * diceValue + 1);
-					results.Add(currentRoll);
-				}
-				double average = results.Average();
-				if (rollCount == 1) {
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = $"**{results[0]}**",
-						Channel = e.Channel,
-						LogMessage = "RollErrorTooManyArgs"
-					});
-				} else if (rollCount <= 20) {
-					var sb = new StringBuilder();
-					sb.AppendLine($"Average: **{average}**");
-					sb.Append("`[");
-					for (int i = 0; i < rollCount; ++i) {
-						sb.Append(results[i]);
-						if (i != rollCount - 1) {
-							sb.Append(", ");
-						}
-					}
-					sb.Append("]`");
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = sb.ToString(),
-						Channel = e.Channel,
-						LogMessage = "RollSuccessLowRoll"
-					});
+			// There must be exactly two terms.
+			if (splitMessage.Length == 2) {
+				if (splitMessage[1].ToLower() == "coin") {
+					await FlipCoin(e);
 				} else {
-					// If more than ten rolls occurred, a 5-number summary is a better way of showing the results.
-					results.Sort();
-					var sb = new StringBuilder();
-					sb.AppendLine($"Average: **{average}**");
-					int halfSize = rollCount / 2;
-					double median = rollCount % 2 == 0 ? (results[halfSize] + results[halfSize - 1]) / 2.0 : results[halfSize];
-					double firstQuart = halfSize % 2 == 0 ? (results[halfSize / 2] + results[halfSize / 2 - 1]) / 2.0 : results[halfSize / 2];
-					double thirdQuart = halfSize % 2 == 0 ? (results[rollCount - halfSize / 2] + results[rollCount - halfSize / 2 - 1]) / 2.0 : results[rollCount - halfSize / 2];
-					sb.Append($"`[{results.First()}, {firstQuart}, {median}, {thirdQuart}, {results.Last()}]`");
-					await BotMethods.SendMessage(this, new SendMessageEventArgs {
-						Message = sb.ToString(),
-						Channel = e.Channel,
-						LogMessage = "RollSuccessHighRoll"
-					});
+					string[] splitRoll = splitMessage[1].Split('d');
+					if (splitRoll.Length == 1) {
+						bool success = int.TryParse(splitRoll[0], out int diceValue);
+						if (success) {
+							if (diceValue > 1000000) {
+								await BotMethods.SendMessage(this, new SendMessageEventArgs {
+									Message = $"You can't roll a {diceValue}-sided die! Please use a lower number (at most 1,000,000).",
+									Channel = e.Channel,
+									LogMessage = "RollErrorSingleTooHigh"
+								});
+							} else if (diceValue >= 2) {
+								await RollDice(e, 1, diceValue);
+							} else {
+								await BotMethods.SendMessage(this, new SendMessageEventArgs {
+									Message = $"You can't roll a {diceValue}-sided die! Please use a higher number (at least 2).",
+									Channel = e.Channel,
+									LogMessage = "RollErrorSingleTooLow"
+								});
+							}
+						} else {
+							await BotMethods.SendMessage(this, new SendMessageEventArgs {
+								Message = $"{splitRoll[0]} is not a valid number!",
+								Channel = e.Channel,
+								LogMessage = "RollErrorSingleInvalidNumber"
+							});
+						}
+					} else if (splitRoll.Length == 2) {
+						bool success1 = int.TryParse(splitRoll[0], out int numDice);
+						if (success1) {
+							if (numDice > 50) {
+								await BotMethods.SendMessage(this, new SendMessageEventArgs {
+									Message = $"You can't roll {numDice} dice! Please use a lower number (at most 50).",
+									Channel = e.Channel,
+									LogMessage = "RollErrorMultipleNumTooHigh"
+								});
+							} else if (numDice >= 1) {
+								bool success2 = int.TryParse(splitRoll[1], out int diceValue);
+								if (success2) {
+									if (diceValue > 1000000) {
+										await BotMethods.SendMessage(this, new SendMessageEventArgs {
+											Message = $"You can't roll a {diceValue}-sided die! Please use a lower number (at most 1,000,000).",
+											Channel = e.Channel,
+											LogMessage = "RollErrorMultipleValueTooHigh"
+										});
+									} else if (diceValue >= 2) {
+										await RollDice(e, numDice, diceValue);
+									} else {
+										await BotMethods.SendMessage(this, new SendMessageEventArgs {
+											Message = $"You can't roll a {diceValue}-sided die! Please use a higher number (at least 2).",
+											Channel = e.Channel,
+											LogMessage = "RollErrorMultipleValueTooLow"
+										});
+									}
+								} else {
+									await BotMethods.SendMessage(this, new SendMessageEventArgs {
+										Message = $"{splitRoll[1]} is not a valid number!",
+										Channel = e.Channel,
+										LogMessage = "RollErrorMultipleValueInvalidNumber"
+									});
+								}
+							} else {
+								await BotMethods.SendMessage(this, new SendMessageEventArgs {
+									Message = $"You can't roll {numDice} dice! Please use a higher number (at least 1).",
+									Channel = e.Channel,
+									LogMessage = "RollErrorMultipleNumTooLow"
+								});
+							}
+						} else {
+							await BotMethods.SendMessage(this, new SendMessageEventArgs {
+								Message = $"{splitRoll[0]} is not a valid number!",
+								Channel = e.Channel,
+								LogMessage = "RollErrorMultipleNumInvalidNumber"
+							});
+						}
+					} else {
+						await BotMethods.SendMessage(this, new SendMessageEventArgs {
+							Message = $"I couldn't determine what you wanted. Check {"?help roll".Code()} for ways to use this command.",
+							Channel = e.Channel,
+							LogMessage = "RollErrorTooManyDs"
+						});
+					}
 				}
+			} else {
+				await BotMethods.SendMessage(this, new SendMessageEventArgs {
+					Message = $"I couldn't determine what you wanted. Check {"?help roll".Code()} for ways to use this command.",
+					Channel = e.Channel,
+					LogMessage = "RollErrorInvalidArgumentCount"
+				});
 			}
 
 			await Task.Delay(0);
+		}
+
+		private async Task RollDice(MessageCreateEventArgs e, int numRolls, int diceValue) {
+			var results = Enumerable.Range(0, numRolls).Select(_ => random.Next(diceValue) + 1).ToList();
+			if (results.Count == 1) {
+				await BotMethods.SendMessage(this, new SendMessageEventArgs {
+					Message = IntToRegionalIndicator(results.Single()),
+					Channel = e.Channel,
+					LogMessage = "RollSuccessSingle"
+				});
+			} else {
+				var sb = new StringBuilder();
+				sb.AppendLine($"The results of the {numRolls} dice-rolls are:");
+				sb.AppendLine("```");
+				for (int i = 0; i < numRolls; ++i) {
+					sb.Append(results[i].ToString().PadLeft(8, ' '));
+					if (i % 5 == 4) {
+						sb.AppendLine();
+					}
+				}
+				sb.AppendLine("\n```");
+				await BotMethods.SendMessage(this, new SendMessageEventArgs {
+					Message = sb.ToString(),
+					Channel = e.Channel,
+					LogMessage = "RollSuccessMultiple"
+				});
+			}
+		}
+
+		private string IntToRegionalIndicator(int x) {
+			if (x >= 10) {
+				return IntToRegionalIndicator(x / 10) + IntToRegionalIndicator(x % 10);
+			} else {
+				return x switch
+				{
+					0 => ":zero:",
+					1 => ":one:",
+					2 => ":two:",
+					3 => ":three:",
+					4 => ":four:",
+					5 => ":five:",
+					6 => ":six:",
+					7 => ":seven:",
+					8 => ":eight:",
+					9 => ":nine:",
+					_ => "?",
+				};
+			}
+		}
+
+		private async Task FlipCoin(MessageCreateEventArgs e) {
+			int result = random.Next(2);
+			if (result == 0) {
+				await BotMethods.SendMessage(this, new SendMessageEventArgs {
+					Message = ":regional_indicator_h::regional_indicator_e::regional_indicator_a::regional_indicator_d::regional_indicator_s:",
+					Channel = e.Channel,
+					LogMessage = "RollSuccessCoinHeads"
+				});
+			} else {
+				await BotMethods.SendMessage(this, new SendMessageEventArgs {
+					Message = ":regional_indicator_t::regional_indicator_a::regional_indicator_i::regional_indicator_l::regional_indicator_s:",
+					Channel = e.Channel,
+					LogMessage = "RollSuccessCoinTails"
+				});
+			}
 		}
 	}
 }
